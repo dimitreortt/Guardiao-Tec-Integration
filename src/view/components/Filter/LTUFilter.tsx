e1a79eaba0865b9e4094e7f83fdfd39187e63a8e
Dimitre Ortt <dimiortt@gmail.com>
2022-07-22 19:58:20 -0300

    Created filter in Itinerary page with TextField Procurar LTU pelo nome

--- src/view/components/Filter/LTUFilter.tsx
+++ src/view/components/Filter/LTUFilter.tsx
@@ -5,21 +5,31 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
-} from '@mui/material';
-import React, { FunctionComponent, useEffect, useState } from 'react';
-import { dispatchSetSelectedLTU } from '../../../application/store/actions/company';
-import { RootState } from '../../../application/store/configureStore';
-import { useSelector } from 'react-redux';
-import { fetchFTs } from '../../../infra/services/fetchFTs';
-import { FT } from '../../../domain/entities/FT';
+  TextField,
+} from "@mui/material";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { dispatchSetSelectedLTU } from "../../../application/store/actions/company";
+import { RootState } from "../../../application/store/configureStore";
+import { useSelector } from "react-redux";
+import { fetchFTs } from "../../../infra/services/fetchFTs";
+import { FT } from "../../../domain/entities/FT";
 
 type Props = {};
 
 export const LTUFilter: FunctionComponent<Props> = ({}) => {
   const [fts, setFTs] = useState<FT[]>([]);
+  const [ltuFilter, setLtuFilter] = useState("");
+  const [filteredFTs, setFilteredFTs] = useState<FT[]>([]);
   const { selectedLTU, adminSelectedCompanyId, userCompanyId } = useSelector(
     (state: RootState) => state.companies
   );
+
+  useEffect(() => {
+    const filteredFTs = fts.filter((ft: FT) => {
+      return ft.values["Nº da Linha"].includes(ltuFilter);
+    });
+    setFilteredFTs(filteredFTs);
+  }, [fts, ltuFilter]);
 
   useEffect(() => {
     fetchFTs(setFTs);
@@ -30,28 +40,32 @@ export const LTUFilter: FunctionComponent<Props> = ({}) => {
     dispatchSetSelectedLTU(LTU);
   };
 
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLtuFilter(event.target.value);
+  };
+
   return (
-    <Box sx={{ position: 'absolute', left: 220, top: 80 }}>
+    <Box sx={{ position: "absolute", left: 220, top: 80, display: "flex" }}>
       <Box sx={{ mb: 1.3 }}>
         <FormControl sx={{ minWidth: 180 }} fullWidth>
-          <InputLabel id='demo-simple-select-helper-label'>LTU</InputLabel>
+          <InputLabel id="demo-simple-select-helper-label">LTU</InputLabel>
           <Select
-            labelId='demo-simple-select-helper-label'
-            id='demo-simple-select-helper'
-            value={selectedLTU || ''}
-            label={'LTU'}
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={selectedLTU || ""}
+            label={"LTU"}
             onChange={handleSelectLTU}
             fullWidth
           >
             {
-              fts &&
-                fts.map((ft: FT) => {
+              filteredFTs &&
+                filteredFTs.map((ft: FT) => {
                   return (
                     <MenuItem
-                      key={ft.values['Nº da FT']}
-                      value={ft.values['Nº da Linha']}
+                      key={ft.values["Nº da FT"]}
+                      value={ft.values["Nº da Linha"]}
                     >
-                      {ft.values['Nº da Linha']}
+                      {ft.values["Nº da Linha"]}
                     </MenuItem>
                   );
                 })
@@ -63,6 +77,15 @@ export const LTUFilter: FunctionComponent<Props> = ({}) => {
             }
           </Select>
         </FormControl>
+      </Box>
+      <Box sx={{ mb: 1.3, ml: 1.3 }}>
+        <TextField
+          label={"Procurar LTU pelo nome"}
+          value={ltuFilter || ""}
+          variant="outlined"
+          onChange={handleFilterChange}
+          fullWidth
+        />
       </Box>
     </Box>
   );
